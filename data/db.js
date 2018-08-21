11cbd860f959f611f54dd427cdd9eb5aaead88c4
MN <m.nkonde.o@gmail.com>
2018-08-21 18:31:37 -0400

    updated db conn info

--- data/db.js
+++ data/db.js
@@ -17,7 +17,9 @@ exports.init = function (callback) {
         // 1. open database connection
         function (cb) {
             console.log("\n** 1. open db");
-            MongoClient.connect(process.env.MONGODB_URI, (err, dbase) => {
+            //var url = local.config.db_config.host_url;
+            MongoClient.connect(url, (err, dbase) => {
+            //MongoClient.connect(process.env.MONGODB_URI, (err, dbase) => {
                 if (err) return cb(err);
                 console.log("**    Connected to server");
                 db = dbase;
